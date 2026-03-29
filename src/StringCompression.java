bc23cfdb92490da5c186cc6a89d64049443873f0
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-29 22:25:40 +0530

    Update StringCompression.java

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,30 +1,37 @@
     import java.util.Scanner;
 
 public class StringCompression {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String S = sc.nextLine();
 
-        String result = "";
+        if (S.length() == 0) {
+            System.out.println(S);
+            return;
+        }
+
+        StringBuilder result = new StringBuilder();
         int count = 1;
 
         for (int i = 1; i <= S.length(); i++) {
             if (i < S.length() && S.charAt(i) == S.charAt(i - 1)) {
                 count++;
             } else {
-                result += S.charAt(i - 1);
-                result += count;
+                result.append(S.charAt(i - 1));
+                result.append(count);
                 count = 1;
             }
         }
 
         if (result.length() < S.length()) {
-            System.out.println(result);
+            System.out.println(result.toString());
         } else {
             System.out.println(S);
         }
     }
+
 }
   
 
