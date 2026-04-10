d872ebffbc4b113207a5c3b933822d57155262da
elena-elsa101 <elenaelsacherian@gmail.com>
2026-04-10 18:47:56 +0530

    Update StringCompression.java

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,28 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        
+        // Check if input exists to avoid errors
+        if (!sc.hasNext()) {
+            return;
+        }
+        
+        String str = sc.next();
+        System.out.println(compress(str));
+    }
+
+    public static String compress(String str) {
+        // A string of length 0 or 1 cannot be compressed to be smaller
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
 
         StringBuilder compressed = new StringBuilder();
+        int count = 0;
 
-        int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+            count++;
 
-        for (int i = 1; i <= s.length(); i++) {
-            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
-                count++;
-            } else {
-                compressed.append(s.charAt(i - 1));
+            // If the next character is different, or we are at the end of the string
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressed.append(str.charAt(i));
                 compressed.append(count);
-                count = 1;
+                count = 0; // Reset counter for the next character
             }
         }
 
-        if (compressed.length() < s.length()) {
-            System.out.print(compressed.toString());
-        } else {
-            System.out.print(s);
-        }
+        String result = compressed.toString();
+
+        // Constraint: Return original if compressed is not smaller
+        return result.length() < str.length() ? result : str;
     }
 }
