a2990faea3eebf10f4b49609105d9a543e01c6ca
elena-elsa101 <elenaelsacherian@gmail.com>
2026-04-10 12:42:22 +0530

    Update StringCompression.java

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,32 +1,35 @@
-    import java.util.Scanner;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.Map;
 
-public class StringCompression {
+public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-
-        StringBuilder compressed = new StringBuilder();
-
-        int count = 1;
-
-        for (int i = 1; i <= s.length(); i++) {
-            if (i < s.length() && s.charAt(i) == s.charAt(i - 1)) {
-                count++;
-            } else {
-                compressed.append(s.charAt(i - 1));
-                compressed.append(count);
-                count = 1;
+        Scanner scanner = new Scanner(System.in);
+        
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\s+");
+            java.util.HashMap<String, Integer> map = new java.util.HashMap<>();
+            
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+                }
             }
+            
+            // Sort by frequency (descending) then alphabetically
+            java.util.List<java.util.Map.Entry<String, Integer>> list = new java.util.ArrayList<>(map.entrySet());
+            list.sort((a, b) -> {
+                int freqCompare = b.getValue().compareTo(a.getValue());
+                if (freqCompare != 0) return freqCompare;
+                return a.getKey().compareTo(b.getKey());
+            });
+            
+            for (java.util.Map.Entry<String, Integer> entry : list) {
+                System.out.print(entry.getKey() + ": " + entry.getValue() + " ");
+            }
+            System.out.println();
         }
-
-        if (compressed.length() < s.length()) {
-            System.out.print(compressed.toString());
-        } else {
-            System.out.print(s);
-        }
+        scanner.close();
     }
 }
-
-
-  
-
